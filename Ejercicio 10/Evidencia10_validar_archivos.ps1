5dac059eebf5fe04c1fa310af85f9094f8a69f46
Javiigxs <erick.casasc@uanl.edu.mx>
2025-09-25 11:51:28 -0600

    Add files via upload

---
+++ Ejercicio 10/Evidencia10_validar_archivos.ps1
@@ -0,0 +1,19 @@
+function Verificar-Archivo {
+    param (
+        [string]$Ruta
+    )
+
+    try {
+               if (Test-Path $Ruta) {
+            Write-Output "El archivo existe: $Ruta"
+        } else {
+            Write-Output "El archivo NO existe: $Ruta"
+        }
+    }
+    catch {
+        Write-Output "Error: $_"
+    }
+    finally {
+        Write-Host "Verificación finalizada para: $Ruta" -ForegroundColor Cyan
+    }
+}
